7236fa551db88bc242dfca6d59173ea4d485b8af
Alan a2n Bouteiller <bouteiller.alan@protonmail.ch>
2022-01-04 01:35:52 +0100

    basic

---
+++ .config/polybar/scripts/env-temp.sh
@@ -0,0 +1,5 @@
+
+export REDSHIFT=off
+export REDSHIFT_TEMP=24800
+export GH_TOKEN=token
+export GH_NAME="account name"
---
+++ .config/polybar/scripts/workspace.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# get the result
+IN=$1
+#IN="%{F#54a0ff}%{F-}+%{F#c8d6e5}%{F-}+%{F#c8d6e5}%{F-}+%{F#576574}%{F-}+%{F#576574}%{F-}+%{F#576574}%{F-}:Spacing Tall:%{F#00ff00} workspace.sh — ~ — Atom %{F-}"
+
+# replace all space by +
+string=$(echo $IN | tr " " "+")
+
+# transform in array
+arrIN=(${string//:/ })
+
+# echo and remove not necessary characters
+echo $(echo ${arrIN[0]} | tr "+" " ")
